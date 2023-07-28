3c19561e9d824628d003cdae838cc2bdf5844484
Drew Richards <98420057+darichards10@users.noreply.github.com>
2023-07-28 18:20:45 -0400

    Added TTFB functionality

--- SiteTestApp/urls.py
+++ SiteTestApp/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from . import views
+from .views import calculate_ttfb
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path('ttfb/', calculate_ttfb, name='ttfb'),
 ]
--- SiteTestApp/views.py
+++ SiteTestApp/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
+import requests
+from django.http import JsonResponse
+from urllib.parse import urlparse
 
+def calculate_ttfb(request):
+    if 'url' in request.GET:
+        url = request.GET['url']
+        if not urlparse(url).scheme:
+            url = 'https://' + url
+        try:
+            response = requests.get(url)
+            response.raise_for_status()  # Raise an exception for 4xx and 5xx status codes
+            ttfb = response.elapsed.total_seconds()
+            return JsonResponse({'ttfb': ttfb})
+        except requests.exceptions.RequestException as e:
+            return JsonResponse({'error': str(e)})
+    else:
+        return JsonResponse({'error': 'URL parameter is missing'})
+    
 def index(request):
     return render(request, 'index.html')
 
